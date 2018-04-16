3ac3ac568e6386ea1256ccf3e9a30a3a090a7893
FranckAchard <36164145+FranckAchard@users.noreply.github.com>
2018-04-16 16:12:17 +0200

    sav exos etape 1

--- script.js
+++ script.js
@@ -5,11 +5,30 @@ function sayHello() {
 sayHello();
 
 // function sum
-function sum() {
+function sum2() {
 	const nb1= parseInt(prompt("saisir 1er nb", ""));
 	const nb2= parseInt(prompt("saisir 2e nb", ""));
 	return nb1+nb2;
 }
 
+/*
 const parg= document.querySelector("p.sum");
-parg.textContent= "Résultat de la somme : " + sum();
+parg.textContent= "Résultat de la somme : " + sum2();
+*/
+
+function compare(a, b) {
+	return a - b;
+}
+
+function sort5() {
+	const tab= [];
+	tab[0]= parseInt(prompt("saisir 1er nb", ""));
+	tab[1]= parseInt(prompt("saisir 2e nb", ""));
+	tab[2]= parseInt(prompt("saisir 3e nb", ""));
+	tab[3]= parseInt(prompt("saisir 4e nb", ""));
+	tab[4]= parseInt(prompt("saisir 5e nb", ""));
+	return tab.sort(compare);
+}
+
+const parg= document.querySelector("p.sort");
+parg.textContent= "Résultat du tri : " + sort5();
